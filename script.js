ffdd6395cf3254f184a51a02dfd6c60ca49ce097
ognjengajic <ognjeng535@gmail.com>
2026-05-05 12:39:18 +0200

    Constructor Functions and the new Operator

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  /*
+  //NOT TO DO
+  this.calcAge = function () {
+    console.log(2045 - this.birthYear);
+  };
+*/
+};
+
+const ognjen = new Person(`Ognjen`, 2001);
+console.log(ognjen);
+
+const john = new Person(`Johnatan`, 1978);
+const jack = new Person(`Jack`, 2002);
+console.log(john, jack);
+
+const jay = `Jay`;
+
+console.log(john instanceof Person);
+console.log(jay instanceof Person);

84fa0f2714f066c7738a98a553b62d2ba2aca53b
ognjengajic <ognjeng535@gmail.com>
2026-05-05 13:25:20 +0200

    Prototypes

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ const jay = `Jay`;
 
 console.log(john instanceof Person);
 console.log(jay instanceof Person);
+
+//Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2045 - this.birthYear);
+};
+
+ognjen.calcAge();
+john.calcAge();
+jack.calcAge();
+
+console.log(ognjen.__proto__);
+console.log(ognjen.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(john));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = `Homo Sapiens`;
+console.log(ognjen, john, jack);
+console.log(ognjen.species, john.species, jack.species);
+
+console.log(ognjen.hasOwnProperty(`firstName`));
+console.log(ognjen.hasOwnProperty(`species`));

f7761668604504e14b5f4db01a43414e4770d85f
ognjengajic <ognjeng535@gmail.com>
2026-05-09 12:27:07 +0200

    Challange #3

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ bmwCL.speedUS = 50;
 console.log(bmwCL.speed);
 */
 //Inheritance Between "Classes" - Constructor Functions
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -254,3 +254,50 @@ console.log(tom instanceof Student);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+//3. Challange
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 20;
+  console.log(`acc ${this.make} speed ${this.speed}`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 10;
+  console.log(`desc ${this.make} speed ${this.speed}`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+const tesla = new EV(`Tesla`, 120, 23);
+tesla.chargeBattery(90);
+console.dir(tesla);
+tesla.brake();
+tesla.accelerate();
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} with a charge of ${this.charge}`,
+  );
+};
+
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
